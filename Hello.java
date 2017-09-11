cc3120579d9fc687240c811ce28709de5eca9930
Jakobhi11 <jakobhill@email.arizona.edu>
2017-09-11 08:34:52 -0700

    Added callSign(), compiled Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 class Hello {
    public static void main(String args[]) {
-      System.out.println("hello");
+      System.out.println("dnajork");
+      System.out.println("jakobhill");
+      int a = callSign();
+      System.out.println(Integer.toString(a));
+   }
+
+   public static int callSign() {
+      return 5;	
    }
 }
