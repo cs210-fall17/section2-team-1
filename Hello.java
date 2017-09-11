30214fa0caef21d48a1c8fccc355bafb3334c16e
David Najork <david@najork.org>
2017-09-11 08:45:54 -0700

    retrieved Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+ass Hello {
+   public static void main(String args[]) {
+      System.out.println("dnajork");
+      System.out.println("jakobhill");
+      int a = callSign();
+      System.out.println(Integer.toString(a));
+   }
+
+   public static int callSign() {
+      return 50;	
+   }
+}
